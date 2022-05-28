6cc635d7cfcad25ab90d25a2d3cbe5d948e4586a
Nadir <nadben0708@gmail.com>
2022-05-28 13:25:55 -0400

    cleaned code and added a few Delete endpoints and handlers
    Deleted WeatherForecast controller

--- AssetManager.Api/Controllers/AssetManagerController.cs
+++ AssetManager.Api/Controllers/AssetManagerController.cs
@@ -1,6 +1,6 @@
 ﻿using AssetManager.Application.Commands;
 using AssetManager.Application.DTO;
-using AssetManager.Application.Queries; 
+using AssetManager.Application.Queries;
 using Microsoft.AspNetCore.Mvc;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
@@ -20,7 +20,12 @@ public class AssetManagerController : Controller
         _logger = logger;
         _mediator = mediator;
     }
-    
+
+    /// <summary>
+    /// this endpoint is used to get the Area created with its assigned Asset.
+    /// </summary>
+    /// <param name="GetAreaQuery"></param>
+    /// <returns></returns>
     // [Authorize(Roles = "SuperAdmin")]
     [Authorize]
     [HttpGet("Area/{AreaId:guid}")]
@@ -30,7 +35,12 @@ public class AssetManagerController : Controller
         var result = await _mediator.Send(query);
         return Ok(result);
     }
-    
+
+    /// <summary>
+    /// This endpoint is used to get the Asset created.
+    /// </summary>
+    /// <param name="GetAssetQuery"></param>
+    /// <returns></returns>
     // [Authorize(Roles = "SuperAdmin")]
     [HttpGet("Asset/{AssetId:guid}")]
     [ProducesResponseType(typeof(AssetDto), 200)]
@@ -40,6 +50,11 @@ public class AssetManagerController : Controller
         return Ok(result);
     }
 
+    /// <summary>
+    /// this endpoint is used to create an Area.
+    /// </summary>
+    /// <param name="CreateAreaCommand"></param>
+    /// <returns></returns>
     // [Authorize]
     [HttpPost("Area")]
     public async Task<IActionResult> Post([FromBody] CreateAreaCommand command)
@@ -47,6 +62,12 @@ public class AssetManagerController : Controller
         var result = await _mediator.Send(command);
         return CreatedAtAction(nameof(Get), new { id = result }, command);
     }
+
+    /// <summary>
+    /// This endpoint is used to create an Asset.
+    /// </summary>
+    /// <param name="CreateAssetCommand"></param>
+    /// <returns></returns>
     // [Authorize]
     [HttpPost("Asset")]
     public async Task<IActionResult> Post([FromBody] CreateAssetCommand command)
@@ -54,7 +75,12 @@ public class AssetManagerController : Controller
         var result = await _mediator.Send(command);
         return CreatedAtAction(nameof(Get), new { id = result }, command);
     }
-    
+
+    /// <summary>
+    /// this endpoint is used to assign an Asset to an Area.
+    /// </summary>
+    /// <param name="AssignAssetToAreaCommand"></param>
+    /// <returns></returns>
     // [Authorize(Roles = "Admin, SuperAdmin")]
     [HttpPost("{AreaId:guid}/Assets/{AssetId:guid}")]
     public async Task<IActionResult> Put([FromBody] AssignAssetToAreaCommand command)
@@ -62,7 +88,12 @@ public class AssetManagerController : Controller
         await _mediator.Send(command);
         return Ok();
     }
-    
+
+    /// <summary>
+    /// This endpoint is used to assign an owner to an Area.
+    /// </summary>
+    /// <param name="AssignOwnerToAreaCommand"></param>
+    /// <returns></returns>
     // [Authorize(Roles = "Admin, SuperAdmin")]
     [HttpPost("{areaid:guid}/Area/{ownerid:guid}")]
     public async Task<IActionResult> Put([FromBody] AssignOwnerToAreaCommand command)
@@ -70,9 +101,33 @@ public class AssetManagerController : Controller
         await _mediator.Send(command);
         return Ok();
     }
-    
+
+    /// <summary>
+    /// This endpoint is used to Assign an owner to an Asset.
+    /// </summary>
+    /// <param name="AssignOwnerToAssetCommand"></param>
+    /// <returns></returns>
     [HttpPost("{AreaId:guid}/Asset/{AssetId:guid}")]
     public async Task<IActionResult> Put([FromBody] AssignOwnerToAssetCommand command)
+    {
+        await _mediator.Send(command);
+        return Ok();
+    }
+
+    /// <summary>
+    /// This endpoint is used to delete an Area.
+    /// </summary>
+    /// <param name="DeleteAreaCommand"></param>
+    /// <returns></returns>
+    [HttpDelete("{AreaId}")]
+    public async Task<IActionResult> Delete([FromBody] DeleteAreaCommand command)
+    {
+        await _mediator.Send(command);
+        return Ok();
+    }
+
+    [HttpDelete("{AssetId}")]
+    public async Task<IActionResult> Delete([FromBody] DeleteAssetCommand command)
     {
         await _mediator.Send(command);
         return Ok();
---
+++ AssetManager.Application/Commands/DeleteAreaCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace AssetManager.Application.Commands;
+
+public record DeleteAreaCommand(Guid AreaId) : IRequest<bool>;
